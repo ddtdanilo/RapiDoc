3022d41abfbc918e4a2cafd0f16bf0aba71b99e1
Ben <Ben>
2021-12-10 16:59:36 -0600

    Customize button radio

--- src/styles/custom-styles.js
+++ src/styles/custom-styles.js
@@ -7,4 +7,11 @@ customize their theme. Simply add your css to this file and yarn build.
 
 export default css`
 
+.toolbar-btn{
+    border-radius: 20px;
+}
+.m-btn{
+    border-radius: 20px;
+}
+
 `;
